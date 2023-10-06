09dbbd7b135beac906511d37ce02f10f736aca88
Zhamilya <145113532+jayme19z@users.noreply.github.com>
2023-10-06 15:29:33 +0600

    Delete Labs/task13.py

--- Labs/task13.py
+++
@@ -1,8 +0,0 @@
-# 13. Количество совпадающих пар
-a = [int(s) for s in input().split()]
-counter = 0
-for i in range(len(a)):
-    for j in range(i + 1, len(a)):
-        if a[i] == a[j]:
-            counter += 1
-print(counter)
