1e9362763a936563df34c9df3f59ba7ecfdc1af3
Zhamilya <145113532+jayme19z@users.noreply.github.com>
2023-10-06 15:29:58 +0600

    Delete Labs/task12.py

--- Labs/task12.py
+++
@@ -1,9 +0,0 @@
-# 12. Вставить элемент
-a = [int(s) for s in input().split()]
-k, C = [int(s) for s in input().split()]
-
-a.append(0)
-for i in range(len(a) - 1, k, -1):
-    a[i] = a[i - 1]
-a[k] = C
-print(' '.join([str(i) for i in a]))
